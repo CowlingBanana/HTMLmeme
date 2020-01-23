dbe95eb1f9493105d318ae9b9d6e94030c83f319
CowlingBanana <bananacode0@gmail.com>
2020-01-23 09:02:21 +0545

    Task Manager App

---
+++ TaskManager/app.js
@@ -0,0 +1,105 @@
+// define ui VARS
+
+const form = document.querySelector("#task-form");
+const taskInput = document.querySelector("#task");
+const filter = document.querySelector("#filter");
+const clearBtn = document.querySelector(".clear-tasks");
+const taskList = document.querySelector(".collection");
+
+// call all event listeners
+loadEventListeners();
+// load all event listeners
+function loadEventListeners() {
+    // Add task Event
+    form.addEventListener("submit", addTask);
+    // Remove Task Event
+    taskList.addEventListener("click", removeTask);
+    // Clear Task Event
+    clearBtn.addEventListener("click", clearTask);
+    //   Filter Tasks
+    filter.addEventListener("keyup", filterTasks);
+}
+
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    const li = document.querySelectorAll('.collection-item');
+    li.forEach(function (task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    })
+}
+
+// Clear Tasks
+function clearTask() {
+    taskList.innerHTML = "";
+}
+
+// Remove Task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains("delete-item")) {
+        if ("Remove Task") {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Add task
+function addTask(e) {
+    if (taskInput.value === "") {
+        alert("Add a Task");
+    } else {
+        // create li
+        const li = document.createElement("li");
+        // Add Class
+        li.className = "collection-item";
+        // Create Text
+        const text = document.createTextNode(taskInput.value);
+        li.appendChild(text);
+
+        // li.appendChild(document.createTextNode(taskInput.value));
+
+        // create link
+        const link = document.createElement("a");
+
+        // Add Class
+        link.className = "delete-item secondary-content";
+
+        // Add Icon HTML
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+
+        // Append Link
+        li.appendChild(link);
+
+        // Append Li to ul
+        taskList.appendChild(li);
+
+        // console.log(li);
+        // console.log(text);
+        // console.log(link);
+        
+        // Store Task in Local Storage
+        storeTaskInLocalStorage(taskInput.value);
+    }
+    // Clear Input
+    taskInput.value = "";
+    e.preventDefault();
+}
+
+// Local Storage
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
